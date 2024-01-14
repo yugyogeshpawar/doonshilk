dcf1fac6f9a59b02d59c7e81d031d60dcc8a2409
Yogesh Pathe <yugyogeshpawar@gmail.com>
2024-01-15 01:26:18 +0530

    updates

--- src/app/about/VideosCrousel.jsx
+++ src/app/about/VideosCrousel.jsx
@@ -1,12 +1,11 @@
 import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import 'swiper/css/scrollbar'; // Add scrollbar style
 
-const VideosCarousel = ({ onSwiper }) => {
+const VideosCrousel = ({ onSwiper }) => {
   const videos = ['https://api.doonsilk.com/image/DoonsilkProductionVideo.mp4', 'https://api.doonsilk.com/image/%E0%A4%B0%E0%A5%87%E0%A4%B6%E0%A4%AE%20%E0%A4%AB%E0%A5%87%E0%A4%A1%E0%A4%B0%E0%A5%87%E0%A4%B6%E0%A4%A8%20%E0%A4%95%E0%A5%87%20%E0%A4%AC%E0%A5%81%E0%A4%A8%20%E0%A4%95%E0%A4%B0%20%E0%A4%A7%E0%A4%BE%E0%A4%97%E0%A5%87%20,%E0%A4%AE%E0%A4%B9%E0%A4%BF%E0%A4%B2%E0%A4%BE%E0%A4%93%E0%A4%82%20%E0%A4%95%E0%A5%87%20%E0%A4%95%E0%A4%A6%E0%A4%AE%20%E0%A4%AC%E0%A5%9D%20%E0%A4%B0%E0%A4%B9%E0%A5%87%C2%A0%E0%A4%B9%E0%A5%88%E0%A4%82%C2%A0%E0%A4%86%E0%A4%97%E0%A5%87.mp4'];
   const swiperRef = useRef(null);
   const [autoplay, setAutoplay] = useState(true);
@@ -25,24 +24,18 @@ const VideosCarousel = ({ onSwiper }) => {
     swiper.autoplay.start(); // Start autoplay initially
   };
 
-  const handleSlideChangeTransitionEnd = () => {
-    if (autoplay) {
+  const handleSlideChange = () => {
+    if (swiperRef.current && swiperRef.current.autoplay && autoplay) {
       setAutoplay(false); // Disable autoplay after the first slide change
       if (swiperRef.current) {
         swiperRef.current.autoplay.stop();
-      }
-
-      // Pause the video when the slide changes
-      const videoElement = document.querySelector(".swiper-slide-active video");
-      if (videoElement && !videoElement.paused) {
-        videoElement.pause();
       }
     }
   };
 
   return (
-    <div style={{ position: "relative", overflow: "hidden" }}>
-      <div style={{ borderRadius: "10px", width: "100%", height: "100%" }}>
+    <div className="relative overflow-hidden">
+      <div className="rounded w-full h-[240px] md:h-[420px] lg:h-[640px]">
         <Swiper
           ref={(swiper) => {
             swiperRef.current = swiper;
@@ -57,19 +50,17 @@ const VideosCarousel = ({ onSwiper }) => {
           navigation
           pagination={{ clickable: true }}
           scrollbar={{ draggable: true }}
-          onSlideChangeTransitionEnd={handleSlideChangeTransitionEnd}
-          autoplay={{ delay: 3000, disableOnInteraction: false }}
+          onSlideChange={handleSlideChange}
         >
           {videos.map((src, idx) => (
             <SwiperSlide key={idx}>
-              <video
-                style={{ width: "100%", height: "100%", borderRadius: "10px", objectFit: "cover" }}
+              <iframe
+                className="w-full h-full absolute rounded object-cover"
                 src={src}
-                title={`Video ${idx + 1}`}
-                controls
-                preload="auto"
-                onLoadedData={handleVideoPlay}
-              ></video>
+                title={`YouTube Video ${idx + 1}`}
+                allowFullScreen
+                onLoad={handleVideoPlay}
+              ></iframe>
             </SwiperSlide>
           ))}
         </Swiper>
@@ -78,4 +69,4 @@ const VideosCarousel = ({ onSwiper }) => {
   );
 };
 
-export default VideosCarousel;
+export default VideosCrousel;
